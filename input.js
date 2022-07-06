81e68ee88beb4e28a445ecbaf5c77040679824e6
MT91-dev <tariqm1991@gmail.com>
2022-07-06 20:33:58 +0000

    Added move logic to handler function and sent data via connection object

--- input.js
+++ input.js
@@ -1,34 +1,37 @@
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
-    process.exit();
-  };
-};
+// const { connect } = require("./client");
+let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
+  
   stdin.on("data", handleUserInput);
-
+  
   return stdin;
 };
 
+
+const handleUserInput = (data) => {
+
+  if (data === '\u0003') {
+    process.exit();
+  } else if (data === '\u0077') {
+    // console.log("W");
+    connection.write('Move: up');
+  } else if (data === '\u0061') {
+    // console.log("A");
+    connection.write('Move: left');
+  } else if (data === '\u0073') {
+    // console.log("S");
+    connection.write('Move: down');
+  } else if (data === '\u0064') {
+    // console.log("D");
+    connection.write('Move: right');
+  }
+};
 module.exports = {
   setupInput,
 };
-
-
-// switch (data) {
-//   case '\u0003':
-//     process.exit();
-//   case 'w':
-//     return 'Move: up';
-//   case 'a':
-//     return 'Move: left';
-//   case 's':
-//     return 'Move: down';
-//   case 'd':
-//     return 'Move: right';
-// }
